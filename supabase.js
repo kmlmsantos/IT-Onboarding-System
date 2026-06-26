00211985e0e8d39eefd47a31fcf18c7d50d5688d
kmlmsantos <kml.msantos@gmail.com>
2026-06-26 16:25:43 +1000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+// supabase.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
+const supabaseUrl = 'YOUR_PROJECT_URL';
+const supabaseKey = 'YOUR_ANON_PUBLIC_KEY';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
